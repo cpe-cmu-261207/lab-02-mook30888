f4f2ab2342e40bec33f3b31d5ba3440e96f2734d
mook30888 <mookkak@gmail.com>
2020-12-14 01:49:03 +0700

    SAVE UR CLASSROM

--- Gpacalc.js
+++ Gpacalc.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded' , function() {
     function update() {
 
         const list = document.createElement('li')
-        list.innerHTML = document.querySelector('#Class-Name').value
+        list.innerHTML = 'Class Name : ' +  document.querySelector('#Class-Name').value +' Semester : ' + document.querySelector('#Semester').value +' Grade : ' + document.querySelector('#Grade').value
         const task = document.querySelector('ul')
         task.append(list)
         return false
